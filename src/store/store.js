b52d8c8ebff86919f25e14f40a76781e99664562
inhobaek-wj <inhobaek.wj@gmail.com>
2019-12-17 10:05:43 +0900

    register vuex

---
+++ src/store/store.js
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+// registration globally
+// it makes possible for every component to access it.
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+
+});
